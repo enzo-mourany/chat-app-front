4e84f1ca2051309c4a0c3c023bd194b89276994f
Enzo Mourany <enzo.mourany@outlook.com>
2022-05-09 22:52:31 +0200

    added signup support and style

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import { Col, Container, Form, Row, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './signup.css';
 
 function Signup() {
     return (
-        <div>
-            Signup
-        </div>
+        <Container>
+            <Row>
+                <Col md={7} className="d-flex align-items-center justify-content-center flex-direction-column">
+                    <Form style={{ width: '80%', maxWidth: 500 }}>
+                        <Form.Group className="mb-3" controlId="formBasicName">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" placeholder="Your name" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Login
+                        </Button>
+                        <div className="py-4">
+                            <p className="text-center">
+                                Don't have an account ? <Link to="/signup">Signup</Link>
+                            </p>
+                        </div>
+                    </Form>
+                </Col>
+                <Col md={5} className="signup__bg"></Col>
+            </Row>
+        </Container>
     )
 }
 
