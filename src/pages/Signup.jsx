57ba8acc82e114af0a8ffb4be78e49d8e0eb6ef5
Enzo Mourany <enzo.mourany@outlook.com>
2022-05-10 08:29:43 +0200

    added signup from with profile img

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,15 +3,28 @@ import { Col, Container, Form, Row, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './signup.css';
 
+function validateImg() { }
+
 function Signup() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
     return (
         <Container>
             <Row>
                 <Col md={7} className="d-flex align-items-center justify-content-center flex-direction-column">
                     <Form style={{ width: '80%', maxWidth: 500 }}>
+                        <h1 className="text-center">Create account</h1>
+                        <div className="signup-profile-pic__container">
+                            <img src="" className="signup-profile-pic" />
+                            <label htmlFor="image-upload" className="image-upload-label">
+                                <i className="fas fa-plus-circle add-picture-icon"></i>
+                            </label>
+                            <input type="file" id="image-upload" hidden accept="image/png, image/jpeg" onChange={validateImg} />
+                        </div>
                         <Form.Group className="mb-3" controlId="formBasicName">
                             <Form.Label>Name</Form.Label>
-                            <Form.Control type="text" placeholder="Your name" />
+                            <Form.Control type="text" placeholder="Your name" onChange={(e) => setName(e.target.value)} />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
@@ -26,11 +39,11 @@ function Signup() {
                             <Form.Control type="password" placeholder="Password" />
                         </Form.Group>
                         <Button variant="primary" type="submit">
-                            Login
+                            Create account
                         </Button>
                         <div className="py-4">
                             <p className="text-center">
-                                Don't have an account ? <Link to="/signup">Signup</Link>
+                                Already have an account ? <Link to="/login">Login</Link>
                             </p>
                         </div>
                     </Form>
