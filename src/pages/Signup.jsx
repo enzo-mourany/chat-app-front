a51c4bcd00cee93e9db8a59bb6ede50893e3bab1
Enzo Mourany <enzo.mourany@outlook.com>
2022-05-10 09:06:08 +0200

    support upload image + check size

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,26 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Col, Container, Form, Row, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './signup.css';
 
-function validateImg() { }
+
 
 function Signup() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
+
+    //image upload states
+    const [image, setImage] = useState(null);
+    const [uploadingImg, setUploadingImg] = useState(false);
+    const [imagePreview, setImagePreview] = useState(null);
+
+    function validateImg() {
+        const file = e.target.files[0];
+        if (file.size >= 1048576) {
+            return alert("Max file size is 1mb");
+        } else {
+            setImage(file);
+            setImagePreview(URL.createObjectURL(file));
+        }
+    }
+
+    function handleSignup(e) {
+        e.preventDefault();
+    }
+
     return (
         <Container>
             <Row>
                 <Col md={7} className="d-flex align-items-center justify-content-center flex-direction-column">
-                    <Form style={{ width: '80%', maxWidth: 500 }}>
+                    <Form style={{ width: '80%', maxWidth: 500 }} onSubmit={handleSignup} >
                         <h1 className="text-center">Create account</h1>
                         <div className="signup-profile-pic__container">
                             <img src="" className="signup-profile-pic" />
                             <label htmlFor="image-upload" className="image-upload-label">
                                 <i className="fas fa-plus-circle add-picture-icon"></i>
                             </label>
-                            <input type="file" id="image-upload" hidden accept="image/png, image/jpeg" onChange={validateImg} />
+                            <input type="file" id="image-upload" hidden accept="image/png, image/jpeg" onChange={validateImg} value={name} />
                         </div>
                         <Form.Group className="mb-3" controlId="formBasicName">
                             <Form.Label>Name</Form.Label>
@@ -28,7 +48,7 @@ function Signup() {
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} value={email} />
                             <Form.Text className="text-muted">
                                 We'll never share your email with anyone else.
                             </Form.Text>
@@ -36,7 +56,7 @@ function Signup() {
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
                         </Form.Group>
                         <Button variant="primary" type="submit">
                             Create account
@@ -55,3 +75,6 @@ function Signup() {
 }
 
 export default Signup
+
+
+// TODO: Make a spline 3D design for the profile picture
