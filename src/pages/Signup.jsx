97d64e98b663fdabd0ce605917485bb1220a85b6
Enzo Mourany <enzo.mourany@outlook.com>
2022-05-10 09:21:43 +0200

    added method which check if no image is upload

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -15,7 +15,7 @@ function Signup() {
     const [uploadingImg, setUploadingImg] = useState(false);
     const [imagePreview, setImagePreview] = useState(null);
 
-    function validateImg() {
+    function validateImg(e) {
         const file = e.target.files[0];
         if (file.size >= 1048576) {
             return alert("Max file size is 1mb");
@@ -24,9 +24,16 @@ function Signup() {
             setImagePreview(URL.createObjectURL(file));
         }
     }
+    async function uploadImage() {
+        const data = new FormData();
+        data.append('file', image);
+        data.append('upload_preset', 'l7bcejcu');
+    }
 
     function handleSignup(e) {
         e.preventDefault();
+        if (!image) return alert('Please upload your profile picture');
+        const url = await uploadImage(image);
     }
 
     return (
@@ -36,7 +43,7 @@ function Signup() {
                     <Form style={{ width: '80%', maxWidth: 500 }} onSubmit={handleSignup} >
                         <h1 className="text-center">Create account</h1>
                         <div className="signup-profile-pic__container">
-                            <img src="" className="signup-profile-pic" />
+                            <img src={imagePreview || botImg} className="signup-profile-pic" />
                             <label htmlFor="image-upload" className="image-upload-label">
                                 <i className="fas fa-plus-circle add-picture-icon"></i>
                             </label>
