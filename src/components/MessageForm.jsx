dea4e06e60be0b65a9d592e63f11538e663718a1
Enzo Mourany <enzo.mourany@outlook.com>
2022-05-11 14:02:11 +0200

    added messageForm support

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { Button, Col, Form, Row } from 'react-bootstrap';
+import './messageForm.css';
 
 function MessageForm() {
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
     return (
-        <div>
-
-        </div>
-    )
+        <>
+            <div className="messages-output"> </div>
+            <Form onSubmit={handleSubmit}>
+                <Col md={11}>
+                    <Form.Group>
+                        <Form.Control type="text" placeholder="Your message"></Form.Control>
+                    </Form.Group>
+                </Col>
+                <Col md={1}>
+                    <Button variant="primary" type="submit" style={{ width: '100%', backgroundColor: 'lightblue' }}>
+                        <i className="fas fa-paper-plane"></i>
+                    </Button>
+                </Col>
+            </Form>
+        </>
+    );
 }
 
-export default MessageForm
+export default MessageForm;
