173287e5ba76e4a2d378870d37ab06ffbfb7eeb2
Enzo Mourany <enzo.mourany@outlook.com>
2022-05-11 13:54:42 +0200

    added differents rooms to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { ListGroup } from 'react-bootstrap';
 
 function Sidebar() {
+    const rooms = ["first room", "second room", "third room"];
     return (
-        <div>
-
-        </div>
-    )
+        <>
+            <h2>Available rooms</h2>
+            <ListGroup>
+                {rooms.map((room, idx) => (
+                    <ListGroup.Item key={idx}>{idx}</ListGroup.Item>
+                ))}
+            </ListGroup>
+        </>
+    );
 }
 
 export default Sidebar
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+import Sidebar from '../components/Sidebar';
+import MessageForm from '../components/MessageForm';
 
 function Chat() {
     return (
-        <div>Chat</div>
+        <Container>
+            <Row>
+                <Col md={4}>
+                </Col>
+                <Col md={8}>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
